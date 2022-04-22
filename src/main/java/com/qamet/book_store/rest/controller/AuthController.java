af8430d67f19bd9b0bb833ff9317f849f6cc93f6
Gamet.Mammadli <gamet.mammadli@vabiss.com>
2022-04-23 00:14:57 +0400

    add(AuthController, DTO): add auth controller, dto to get access token

---
+++ src/main/java/com/qamet/book_store/rest/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.qamet.book_store.rest.controller;
+
+import com.qamet.book_store.rest.dto.AuthenticationDTO;
+import com.qamet.book_store.rest.dto.JWTResponseDTO;
+import com.qamet.book_store.security.TokenProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/authentication")
+@AllArgsConstructor
+public class AuthController {
+
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    @PostMapping
+    public ResponseEntity<JWTResponseDTO> authenticate(@Valid @RequestBody AuthenticationDTO dto) {
+        authenticationManagerBuilder.getObject().authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword())
+        );
+
+        String token = tokenProvider.createToken(dto.getUsername());
+        JWTResponseDTO jwtResponseDTO = new JWTResponseDTO();
+        jwtResponseDTO.setAccessToken(token);
+        return ResponseEntity.ok(jwtResponseDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> testRole() {
+        return ResponseEntity.ok(null);
+    }
+}
---
+++ src/main/java/com/qamet/book_store/rest/dto/JWTResponseDTO.java
@@ -0,0 +1,10 @@
+package com.qamet.book_store.rest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JWTResponseDTO {
+    private String accessToken;
+}
