a048e24e3d4a25d961822fae8847091e35da98f6
Gamet.Mammadli <gamet.mammadli@vabiss.com>
2022-04-23 06:30:10 +0400

    update(RoleRepository): add Repository annotation

--- src/main/java/com/qamet/book_store/repository/RoleRepository.java
+++ src/main/java/com/qamet/book_store/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package com.qamet.book_store.repository;
 import com.qamet.book_store.entity.Role;
 import com.qamet.book_store.entity.enumeration.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     Role findByName(RoleName name);
