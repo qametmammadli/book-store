96bc16a7d415e375e1c22b8234181a13ea592857
Gamet.Mammadli <gamet.mammadli@vabiss.com>
2022-04-23 21:32:48 +0400

    add(AuthorController): add author controller, service, dto

---
+++ src/main/java/com/qamet/book_store/rest/controller/AuthorController.java
@@ -0,0 +1,14 @@
+package com.qamet.book_store.rest.controller;
+
+import com.qamet.book_store.rest.dto.AuthorDTO;
+import com.qamet.book_store.service.AuthorService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/authors")
+public class AuthorController extends GenericController<AuthorDTO> {
+    public AuthorController(AuthorService authorService) {
+        super(authorService);
+    }
+}
