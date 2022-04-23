7028b12303f926ddf19ef5162cc0ca90715706fa
Gamet.Mammadli <gamet.mammadli@vabiss.com>
2022-04-23 06:29:38 +0400

    add(Book, Author): add book, author tables, entities, repositories

---
+++ src/main/java/com/qamet/book_store/entity/Book.java
@@ -0,0 +1,53 @@
+package com.qamet.book_store.entity;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "books")
+public class Book extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "publisher_id")
+    private Long publisherId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "publisher_id", insertable = false, updatable = false)
+    @ToString.Exclude
+    private User publisher;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id", nullable = false, updatable = false),
+            inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id", nullable = false, updatable = false))
+    private Set<Author> authors = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Book)) {
+            return false;
+        }
+        return id != null && id.equals(((Book) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
