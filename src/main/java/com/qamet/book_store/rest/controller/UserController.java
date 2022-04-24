e5013fc5a823fbbc3b770410c77bd4be7c0a9aa5
Gamet.Mammadli <gamet.mammadli@vabiss.com>
2022-04-24 22:45:20 +0400

    update(UserController):add new get by id, delete, add publisher role services

--- src/main/java/com/qamet/book_store/rest/controller/UserController.java
+++ src/main/java/com/qamet/book_store/rest/controller/UserController.java
@@ -2,13 +2,26 @@ package com.qamet.book_store.rest.controller;
 
 import com.qamet.book_store.rest.dto.UserDTO;
 import com.qamet.book_store.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController extends GenericController<UserDTO> {
+
+    private final UserService userService;
+
     public UserController(UserService userService) {
         super(userService);
+        this.userService = userService;
+    }
+
+    @PutMapping("/{id}/add_publisher_role")
+    public ResponseEntity<Void> addPublisherRole(@PathVariable Integer id) {
+        userService.addPublisherRole(id);
+        return ResponseEntity.ok().build();
     }
 }
