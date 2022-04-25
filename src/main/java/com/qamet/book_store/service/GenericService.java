cbad2b314ed41dec66fd454b7934af6c903ff28c
Gamet.Mammadli <gamet.mammadli@vabiss.com>
2022-04-25 06:44:07 +0400

    update(UserService): add new method find by username

--- src/main/java/com/qamet/book_store/service/GenericService.java
+++ src/main/java/com/qamet/book_store/service/GenericService.java
@@ -3,7 +3,6 @@ package com.qamet.book_store.service;
 import com.qamet.book_store.rest.dto.AbstractDTO;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface GenericService<DTO extends AbstractDTO> {
     void save(DTO dto);
@@ -11,8 +10,6 @@ public interface GenericService<DTO extends AbstractDTO> {
     List<DTO> findAll();
 
     DTO findById(Integer id);
-
-    Optional<DTO> update(Integer id, DTO dto);
 
     void delete(Integer id);
 }
