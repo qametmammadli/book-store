9b240a5405bf4ed63947c2450d15bd1eb13b450d
Qamet Mammadli <qametmammadli7@gmail.com>
2022-10-29 04:39:39 +0400

    update(kafka):change kafka image, produce book dto object and consume book dto object

--- src/main/java/com/qamet/book_store/config/consumer/KafkaConsumerConfig.java
+++ src/main/java/com/qamet/book_store/config/consumer/KafkaConsumerConfig.java
@@ -1,5 +1,6 @@
 package com.qamet.book_store.config.consumer;
 
+import com.qamet.book_store.rest.dto.BookDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,18 +24,24 @@ public class KafkaConsumerConfig {
     private String groupId;
 
 //    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
+    public ConsumerFactory<String, BookDTO> consumerFactory() {
+
+        var deserializer = new JsonDeserializer<>(BookDTO.class);
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.addTrustedPackages("*");
+        deserializer.setUseTypeMapperForKey(true);
+
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        return new DefaultKafkaConsumerFactory<>(configProps);
+        return new DefaultKafkaConsumerFactory<>(configProps, new StringDeserializer(), deserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> myKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, BookDTO> myKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, BookDTO> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
